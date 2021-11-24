9502bc9242e1d5da37a58f99951b6764c9b4b273
gossh520 <2044651+gossh520@user.noreply.gitee.com>
2021-11-24 09:51:46 +0000

    add qxapp.js.

---
+++ qxapp.js
@@ -0,0 +1,11 @@
+/*
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+注册地址：https://xinggeculture.com/#/register
+需要实名认证后绑定支付宝
+qxPhone:手机号#密码
+export qxPhone='手机号#密码'
+boxjs地址:https://gitee.com/gossh520/script/raw/master/byxiaopeng.boxjs.json
+*/
+// [task_local]
+//#千熙
+// 9 8,20 * * * https://gitee.com/gossh520/script/raw/master/qxapp.js, tag=千熙, enabled=true
