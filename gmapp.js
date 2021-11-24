185f7031db2f40e1626a3a20e8235c3475554e08
gossh520 <2044651+gossh520@user.noreply.gitee.com>
2021-11-24 09:54:40 +0000

    add gmapp.js.

---
+++ gmapp.js
@@ -0,0 +1,11 @@
+/*
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+注册地址：https://shanxixincensm.com/#/register
+需要实名认证后绑定支付宝
+gmPhone:手机号#密码
+export gmPhone='手机号#密码'
+boxjs地址:https://gitee.com/gossh520/script/raw/master/byxiaopeng.boxjs.json
+*/
+// [task_local]
+//#国迈
+// 10 8,20 * * * https://gitee.com/gossh520/script/raw/master/gmapp.js, tag=国迈, enabled=true
