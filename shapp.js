b1ace58a5c50ec2f57f27ff4b647a541dbf5242e
gossh520 <2044651+gossh520@user.noreply.gitee.com>
2021-11-24 09:53:17 +0000

    add shapp.js.

---
+++ shapp.js
@@ -0,0 +1,11 @@
+/*
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+注册地址：http://xashangdian.com/#/register
+需要实名认证后绑定支付宝
+shPhone:手机号#密码
+export shPhone='手机号#密码'
+boxjs地址:https://gitee.com/gossh520/script/raw/master/byxiaopeng.boxjs.json
+*/
+// [task_local]
+//#苏汇
+// 9 11 * * * https://gitee.com/gossh520/script/raw/master/shapp.js, tag=苏汇, enabled=true
